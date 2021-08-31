8bf96958eb2038b7256872cc77b40659ec100e5c
Defne Doken <icookiecat22@Gmail.com>
2021-08-31 14:48:26 -0400

    Initial Commit

---
+++ Tots/TotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotsApp.swift
+//  Tots
+//
+//  Created by Defne Doken on 8/31/21.
+//
+
+import SwiftUI
+
+@main
+struct TotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
